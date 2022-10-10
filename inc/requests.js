70224861a6c65f66ecedb50ec9fcc11ba4155f73
Sourabh Burse <sourabh.burse@gmail.com>
2022-10-10 14:56:04 +0400

    Added operation to the requests and updated the publish topic

--- inc/requests.js
+++ inc/requests.js
@@ -2,24 +2,24 @@ const requestTypes = {
     'Energy Profile': 'meter/energyprofile',
     'Load Profile1': 'meter/loadprofile1',
     'Water Profile': 'meter/waterprofile',
-    'Max Demand':'meter/maxdemand',
-    'Load Profile2':'meter/loadprofile2',
-    'Instrumentation Profile':'meter/instprofile',
-    'Instantaneous Values':'meter/instvalue',
-    'Billing Profile':'meter/billingprofile',
-    'Clock Time':'meter/clocktime',
-    'Get Profile Interval':'gateway/gwprofiletime',
-    'Reconnection':'meter/reconnection',
-    'Switch Status':'meter/switchstatus',
-    'Max Demand Reset':'meter/mdr',
-    'Get Gateway Clock Time':'gateway/gwclocktime',
-    'Gateway Sync':'gateway/gwsync',
-    'Clear Alarms':'gateway/clearalarms',
-    'Meter Status':'gateway/meterstatus',
-    'Gateway Nameplate':'gateway/nameplate',
-    'Gateway Ping':'gateway/ping',
-    'Meter Ping':'meter/ping',
-    'Meter Nameplate':'gateway/nameplate'
+    'Max Demand': 'meter/maxdemand',
+    'Load Profile2': 'meter/loadprofile2',
+    'Instrumentation Profile': 'meter/instprofile',
+    'Instantaneous Values': 'meter/instvalue',
+    'Billing Profile': 'meter/billingprofile',
+    'Clock Time': 'meter/clocktime',
+    'Get Profile Interval': 'gateway/gwprofiletime',
+    'Reconnection': 'meter/reconnection',
+    'Switch Status': 'meter/switchstatus',
+    'Max Demand Reset': 'meter/mdr',
+    'Get Gateway Clock Time': 'gateway/gwclocktime',
+    'Gateway Sync': 'gateway/gwsync',
+    'Clear Alarms': 'gateway/clearalarms',
+    'Meter Status': 'gateway/meterstatus',
+    'Gateway Nameplate': 'gateway/nameplate',
+    'Gateway Ping': 'gateway/ping',
+    'Meter Ping': 'meter/ping',
+    'Meter Nameplate': 'gateway/nameplate'
 };
 
 function addDevices(obj) {
@@ -42,7 +42,7 @@ function addParameters(obj) {
     return obj;
 }
 
-function createBasicRequest() {
+function createBasicRequest(operation) {
     // document.getElementById("copyBtn").style.display="block";
     let obj = new Object();
     let gw_sn = document.getElementById("gateway_list").value;
@@ -51,32 +51,35 @@ function createBasicRequest() {
     obj.time = getUnixTime(Date.now());
     obj.gWsn = gw_sn;
     obj.gWip = gw_ip;
+    obj.operation = operation;
     return obj;
 }
 
-function requestType1() {
+function requestType1(operation) {
     let req = document.getElementById("req");
-    let obj = createBasicRequest();
+    let obj = createBasicRequest(operation);
     req.innerHTML = JSON.stringify(obj, undefined, 2);
+    return obj;
 }
 
-function requestType2(flag) {
-    let obj = createBasicRequest();
+function requestType2(operation, flag) {
+    let req = document.getElementById("req");
+    let obj = createBasicRequest(operation);
     obj = addDevices(obj);
     if (flag) return obj;
     req.innerHTML = JSON.stringify(obj, undefined, 2);
 }
 
-function requestType3() {
+function requestType3(operation) {
     let req = document.getElementById("req");
-    let obj = requestType2(1);
+    let obj = requestType2(operation, 1);
     obj = addParameters(obj);
     req.innerHTML = JSON.stringify(obj, undefined, 2);
 }
 
 function requestReconnection() {
     let req = document.getElementById("req");
-    let obj = createBasicRequest();
+    let obj = createBasicRequest('reconnection');
     obj.type = "DISCONNECTION";
     obj.mode = "Mode4";
     obj = addDevices(obj);
@@ -85,7 +88,7 @@ function requestReconnection() {
 
 function requestGatewaySync() {
     let req = document.getElementById("req");
-    let obj = createBasicRequest();
+    let obj = createBasicRequest('gwsynchronization');
     obj.date = getUnixTime(Date.now());
     req.innerHTML = JSON.stringify(obj, undefined, 2);
 }
@@ -95,31 +98,31 @@ function createRequest() {
     document.getElementById("reqname").innerHTML = request_name;
     switch (String(request_name)) {
         case "Energy Profile":
-            requestType2();
+            requestType2('energyprofile');
             break;
         case "Load Profile1":
-            requestType3();
+            requestType3('loadprofile1');
             break;
         case "Water Profile":
-            requestType2();
+            requestType2('waterprofile');
             break;
         case "Max Demand":
-            requestType2();
+            requestType2('maxdemand');
             break;
         case "Load Profile2":
-            requestType3();
+            requestType3('loadprofile2');
             break;
         case "Instrumentation Profile":
-            requestType3();
+            requestType3('instrumentationprofile');
             break;
         case "Instantaneous Values":
-            requestType3();
+            requestType3('instvalues');
             break;
         case "Billing Profile":
-            requestType3();
+            requestType3('billingprofile');
             break;
         case "Clock Time":
-            requestType2();
+            requestType2('clocktime');
             break;
         case "Get Profile Interval":
             break;
@@ -127,7 +130,7 @@ function createRequest() {
             requestReconnection();
             break;
         case "Switch Status":
-            requestType2();
+            requestType2('switchstatus');
             break;
         case "Max Demand Reset":
             break;
@@ -138,22 +141,22 @@ function createRequest() {
             requestGatewaySync();
             break;
         case "Clear Alarms":
-            requestType2();
+            requestType2('clearalarms');
             break;
         case "Meter Status":
-            requestType2();
+            requestType2('meterstatus');
             break;
         case "Gateway Nameplate":
             requestType1();
             break;
         case "Gateway Ping":
-            requestType1();
+            requestType1('gwnameplate');
             break;
         case "Meter Ping":
-            requestType2();
+            requestType2('meterping');
             break;
         case "Meter Nameplate":
-            requestType2();
+            requestType2('meternameplate');
             break;
     }
 }
